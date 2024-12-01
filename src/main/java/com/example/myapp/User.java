111e37d441983f7bff5b85413cd91177eba612f5
routsonali80@gmail.com <routsonali80@gmail.com>
2024-12-01 21:50:18 +0530

    Add User class with authentication method

---
+++ src/main/java/com/example/myapp/User.java
@@ -0,0 +1,13 @@
+public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public boolean authenticate(String inputPassword) {
+        return this.password.equals(inputPassword);
+    }
+}
